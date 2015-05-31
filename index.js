2df2f1cdabc920bcf548b0fac8c0b19663015937
Arjun Madan <amadan2@ncsu.edu>
2015-05-31 00:52:34 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+
+var accountSid = 'AC9f1419406334942728bd3d698ffc97d6'
+var authToken = '946f54caf4c6112b138d5ecf2ca1ba0f'
+var client = require('twilio')(accountSid, authToken);
+
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
