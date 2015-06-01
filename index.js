b0106d0e9b90448c3e8bf585c432b711cff91f00
Arjun Madan <amadan2@ncsu.edu>
2015-06-01 01:36:35 -0400

    Code structure set up

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+//Required modules.
 var express = require('express');
+var message = require('./message.js');
 var app = express();
 
-
+//Twilio authentication
 var accountSid = 'AC9f1419406334942728bd3d698ffc97d6'
 var authToken = '946f54caf4c6112b138d5ecf2ca1ba0f'
 var client = require('twilio')(accountSid, authToken);
 
-
+//Server setup
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+//Twilio processing
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //Gets the content of the text message.
+  var text = request.query.Body;
+  console.log(text);
+
+  //Sends the message to the Spark Core.
+  message.sendMessage(text);
+  
 });
 
 app.listen(app.get('port'), function() {
---
+++ message.js
@@ -0,0 +1,13 @@
+//Required modules.
+var spark = require('spark');
+
+//Authentication
+spark.login({accessToken: '9baba2529d3ddd96afa843cf1c2819c30ad41cff'});
+
+
+//Function that sends the message to the Spark Core
+function sendMessage(text) {
+
+}
+
+exports.sendMessage = sendMessage;
